37351d6cef7edc6b3d16bd929103a196a9edb7f4
pthomas0309 <preston.thomas355@gmail.com>
2021-09-26 14:22:31 -0500

    create function to calculate number of valleys travelled in a hike

--- interviewPractice/countingValleys.js
+++ interviewPractice/countingValleys.js
@@ -19,5 +19,62 @@ string path: a string describing the path
 
 function countingValleys(steps, path) {
     // Write your code here
+    console.log('tracking hike');
 
+    // create array to store the distance
+    // traveled from sea level (0)
+    let unitsFromBase = 0;
+
+    // create variable to store the 
+    // number of valleys travelled in a hike
+    let valleysTravelled = 0;
+
+    // loop through the path string 
+    for (let i = 0; i < path.length; i++ ) {
+
+        /* conditional validates the uphill or 
+        downhill direction and adjusts unitsFromBase
+        accordingly */
+
+        // case: character of string at current index is U (uphill)
+        if (path[i] != 'D') {
+            console.log('in if, going up a mountain.');
+
+            // add one to the last index of unitsFromBase
+            // and push that number to the array
+            unitsFromBase++;
+
+        }// end if
+
+        // case: character of string at current index is D (downhill)
+        // and the hiker is at sea-level
+        else if (path[i] != 'U' && unitsFromBase === 0) {
+            console.log('in else if, going down the valley');
+
+            // subtract one from the last index of unitsFromBase
+            // then push that to unitsFromBase
+            unitsFromBase--;
+
+            // add one to the number of valleys travelled
+            valleysTravelled++;
+
+        } // end else if
+
+        // case: character of string at current index 
+        // is D but the hiker is not at sea-level
+        else {
+            console.log('in else, traveling the path');
+
+            // subtract one from the last index of unitsFromBase
+            // then push that to unitsFromBase
+            unitsFromBase--;
+
+        }// end else
+
+    } // end path loop
+
+        console.log(unitsFromBase)
+    
+    // return the number of valleys travelled
+    return valleysTravelled;
 }
